f099908b49b1a87dd41e8ca3eca05968118daa52
Joseph Cooper <cooper.joseph@gmail.com>
2023-02-28 16:44:38 +0000

    test: Improve testing of image

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -eo pipefail
+
 ##Script to test that docker image runs correctly
 [ -z "$1" ] && echo "First argument must be image to run" && exit 1
 
@@ -10,8 +13,30 @@ function cleanup() {
 
 trap cleanup SIGTERM SIGINT
 
-docker run -d --name emu "$1"
+auth_port=9099
+firestore_port=8080
 
-timeout 60s bash -c 'while [ "$(docker inspect -f {{.State.Health.Status}} emu)" != "healthy" ]; do sleep 2;	done'
+docker run \
+  --publish "$auth_port":"$auth_port" \
+  --publish "$firestore_port":"$firestore_port" \
+	--detach \
+	--name emu \
+	"$image"
+
+function wait_for_emulator() {
+  local health
+  while [[ "$health" != "healthy" ]]
+  do
+    sleep 2
+    health=$(docker inspect -f '{{.State.Health.Status}}' emu)
+    echo "$(date -Iseconds): $health"
+  done
+}
+
+export -f wait_for_emulator
+timeout --foreground 60s bash -c wait_for_emulator
+
+curl localhost:"$auth_port"
+curl localhost:"$firestore_port"
 
 cleanup
