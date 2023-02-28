a3bcfa629892772e523343e32514115cc69e36a1
Joseph Cooper <cooper.joseph@gmail.com>
2023-02-28 16:44:47 +0000

    fix: Wait for all emulators ready in healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,4 +4,5 @@ set -o nounset
 url="http://localhost"
 auth_port="${1:-9099}"
 firestore_port="${2:-8080}"
-curl -fs "$url:${auth_port}" || curl -fs "$url:${firestore_port}" || exit 1
+curl -fs "$url:${auth_port}" ||  exit 1
+curl -fs "$url:${firestore_port}" || exit 2
